60c3eece0ade4a8c50ebdcd0fe4bb2ecdad09bbc
Jonathon <jwbialas@gmail.com>
2022-06-28 16:10:30 -0500

    commit tree 827e935bc2382cab8822c316577af15481d529ed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun (input ='roller-skate' ){
+     return "This Saturday, I want to " + input + "!";
+}
+function mondayWork (input = 'go to the office'){
+    return "This Monday, I will " + input+ ".";
+}
+function wrapAdjective (icon,innerput){
+     let y = function (innerput ="special"){
+        let x = "You are " + icon + innerput + icon + "!"  ;
+        return x;
+    } 
+    return y;
+}
